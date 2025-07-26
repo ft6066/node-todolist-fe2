6eeee74911d22158c9ff994a7c7f041ae56c662c
Seung Hwang <2018066@naver.com>
2025-07-26 17:52:33 +0900

    로그인/회원가입 창에 에러메시지 css효과 적용

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,64 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../utils/api";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const loginSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await api.post("/user/login", { email, password });
+      if (response.status === 200) {
+        setUser(response.data.user);
+        sessionStorage.setItem("token", response.data.token);
+        api.defaults.headers["authorization"] = "Bearer" + response.data.token;
+        setError("");
+        navigate("/");
+      } else {
+        throw new Error(response.data.error);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
     <div className="display-center">
-      <Form className="login-box">
+      {error && (
+        <div
+          className="alert alert-danger d-flex align-items-center"
+          role="alert"
+        >
+          {error}
+        </div>
+      )}
+      <Form className="login-box" onSubmit={loginSubmit}>
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </Form.Group>
         <div className="button-box">
           <Button type="submit" className="button-primary">
